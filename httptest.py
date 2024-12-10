f67011eeb3f483e0e52e1e2c56e68a08442dd440
Joshua <joshuavdmeij@gmail.com>
2024-12-10 19:57:14 +0100

    first tests done
    *added http tests file
    *tested API calls

---
+++ httptest.py
@@ -0,0 +1,13 @@
+GET http://192.168.2.12:5000/api/games
+
+#####
+
+POST http://192.168.2.12:5000/api/games
+content-type: application/json
+
+{
+    "id": 4,
+    "videoId": 100004
+}
+
+######
--- routes.py
+++ routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from models import Game
 from azure.storage.blob import BlobServiceClient
 import os
+import json
 
 routes = Blueprint('routes', __name__)
 
@@ -9,11 +10,20 @@ routes = Blueprint('routes', __name__)
 #AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 #BLOB_SERVICE_CLIENT = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
-games = []
+#dummy data
+games = [
+    {'id': 1, 'videoId': 100001},
+    {'id': 2, 'videoId': 100002}
+]
+#gamenew = Game(3,100003)
+#games.append(gamenew.__dict__)
 memorygames = []
 objectgames = []
 findobjectgames = []
 pointatpicturegames = []
+
+#######################################################################
+#GAMES
 
 #Get all Games
 @routes.route('/api/games', methods=['GET'])
@@ -21,16 +31,24 @@ def getGames():
     #Connect to Azure 
     #Get games
     #Return them into list based on Model
-    return jsonify(games)
+    try:
+        return jsonify(games)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #Post new Game
 @routes.route('/api/games', methods=['POST'])
 def addGame():
-    id = request.json('id')
-    videoId = request.json('videoId')
-    new_game = Game(id,videoId)
-    games.append(new_game.__dict__)
-    return jsonify(new_game.__dict__)
+    try:
+        # id = request.get_json['id']
+        # videoId = request.get_json['videoId']
+        # new_game = Game(id,videoId)
+        # games.append(new_game.__dict__)
+        games.append(request.get_json())
+        return '', 204
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 
 #Get Game by video id and type
 @routes.route('/api/games/<videoId>/<type>', methods=['GET'])
@@ -45,10 +63,20 @@ def getGameByVideoAndType(videoId, type):
 @routes.route('/api/games/<gameId>', methods=['PUT'])
 def updateGame(gameId):
     #update game id
-    return jsonify()
+    try:
+        return jsonify(gameId)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 #Delete Game by Game ID
 @routes.route('/api/games/<gameId>', methods=['DELETE'])
 def deleteGame(gameId):
     #delete Game
-    return jsonify()
+    try:
+        return jsonify(gameId)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+#######################################################################
+#MEMORY GAME
+
